bc0010cdf7afb010e0cb60cdf694e9dabb9b38ef
Matei Stanca <i@ambientimpact.com>
2023-06-29 14:50:32 -0400

    Added basic hook and service to pass early supporter field to Discourse.

---
+++ src/Service/SsoUserData.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\omnipedia_discourse\Service;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\user\UserStorageInterface;
+use Drupal\omnipedia_discourse\Service\SsoUserDataInterface;
+
+/**
+ * The Omnipedia Discourse SSO user data service.
+ */
+class SsoUserData implements SsoUserDataInterface {
+
+  /**
+   * The name of the Drupal user entity field indicating an early supporter.
+   */
+  protected const EARLY_SUPPORTER_DRUPAL_FIELD_NAME = 'field_early_supporter';
+
+  /**
+   * The name of the Discourse SSO field indicating an early supporter.
+   */
+  protected const EARLY_SUPPORTER_DISCOURSE_FIELD_NAME =
+    'custom.early_supporter';
+
+  /**
+   * The Drupal user entity storage.
+   *
+   * @var \Drupal\user\UserStorageInterface
+   */
+  protected UserStorageInterface $userStorage;
+
+  /**
+   * Constructs this service object; saves dependencies.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The Drupal entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface  $entityTypeManager) {
+    $this->userStorage = $entityTypeManager->getStorage('user');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterParameters(array $parameters): void {
+
+    /** @var \Drupal\user\UserInterface|null */
+    $user = $this->userStorage->load($parameters['external_id']);
+
+    if (!\is_object($user)) {
+      return;
+    }
+
+    try {
+
+      $fieldValue = $user->get(
+        self::EARLY_SUPPORTER_DRUPAL_FIELD_NAME
+      )->get(0)->getString();
+
+    // @todo Log this?
+    } catch (\Exception $exception) {
+
+      $parameters[self::EARLY_SUPPORTER_DISCOURSE_FIELD_NAME] = false;
+
+      return;
+
+    }
+
+    $parameters[
+      self::EARLY_SUPPORTER_DISCOURSE_FIELD_NAME
+    ] = (bool) $fieldValue;
+
+  }
+
+}
---
+++ src/Service/SsoUserDataInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\omnipedia_discourse\Service;
+
+/**
+ * The Omnipedia Discourse SSO user data service interface.
+ */
+interface SsoUserDataInterface {
+
+  /**
+   * Alter Discourse SSO parameters.
+   *
+   * @param array $parameters
+   *
+   * @see \Drupal\discourse_sso\Controller\DiscourseSsoController::validate()
+   *   Builds parameters and invokes \hook_discourse_sso_parameters_alter().
+   */
+  public function alterParameters(array $parameters): void;
+
+}
