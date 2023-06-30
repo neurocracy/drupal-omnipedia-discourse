3d2c88f67e4650d3c6653aa50902be4d3817986f
Matei Stanca <i@ambientimpact.com>
2023-06-30 12:02:09 -0400

    Work-in-progress alter test.

---
+++ tests/src/Functional/SsoUserDataAlterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\omnipedia_discourse\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Drupal\omnipedia_discourse\Service\SsoUserDataInterface;
+
+/**
+ * Tests for the Omnipedia Discourse SSO user data service alter hook.
+ *
+ * @group omnipedia_discourse
+ */
+class SsoUserDataAlterTest extends BrowserTestBase {
+
+  /**
+   * The name of the Drupal user entity field indicating an early supporter.
+   */
+  protected const EARLY_SUPPORTER_DRUPAL_FIELD_NAME = 'field_early_supporter';
+
+  /**
+   * The name of the Discourse SSO field indicating an early supporter.
+   */
+  protected const EARLY_SUPPORTER_DISCOURSE_FIELD_NAME = 'custom.user_field_1';
+
+  /**
+   * The Omnipedia Discourse SSO user data service.
+   *
+   * @var \Drupal\omnipedia_discourse\Service\SsoUserDataInterface
+   */
+  protected SsoUserDataInterface $ssoUserData;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['omnipedia_discourse'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+
+    parent::setUp();
+
+    $this->ssoUserData = $this->container->get(
+      'omnipedia_discourse.sso_user_data'
+    );
+
+  }
+
+  /**
+   * Test that our parameters alter hook works as intended.
+   */
+  public function testAlterParameters(): void {
+
+    FieldStorageConfig::create([
+      'entity_type' => 'user',
+      'field_name'  => self::EARLY_SUPPORTER_DRUPAL_FIELD_NAME,
+      'type'        => 'boolean',
+    ])->save();
+
+    FieldConfig::create([
+      'entity_type' => 'user',
+      'field_name'  => self::EARLY_SUPPORTER_DRUPAL_FIELD_NAME,
+    ])->save();
+
+    /** @var \Drupal\user\UserInterface */
+    $user = $this->drupalCreateUser([], null, false, [
+      self::EARLY_SUPPORTER_DRUPAL_FIELD_NAME => 1,
+    ]);
+
+    $this->assertEquals(1, $user->get(
+      self::EARLY_SUPPORTER_DRUPAL_FIELD_NAME
+    )->get(0)->getValue(), 'whoops');
+
+  }
+
+}
